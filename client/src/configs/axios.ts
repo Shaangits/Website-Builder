69811bca02bc05c5b352740127d5f44a77346f63
Shaangits <159615417+Shaangits@users.noreply.github.com>
2026-02-22 10:38:13 +0530

    Update axios baseURL to production endpoint

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASEURL ,
+    baseURL: "https://website-builder-backend-d5ya.onrender.com" ,
     withCredentials: true
 })
 
